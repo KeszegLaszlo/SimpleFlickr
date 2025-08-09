84ecd4ec34d4e891c0ad48a48f11d300abb39af1
Keszeg László <keszeglaszlof@gmail.com>
2025-08-09 10:36:07 +0200

    Initial Commit

---
+++ SimpleFlickr/SimpleFlickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFlickrApp.swift
+//  SimpleFlickr
+//
+//  Created by Keszeg László on 2025. 08. 09..
+//
+
+import SwiftUI
+
+@main
+struct SimpleFlickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleFlickrTests/SimpleFlickrTests.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleFlickrTests.swift
+//  SimpleFlickrTests
+//
+//  Created by Keszeg László on 2025. 08. 09..
+//
+
+import Testing
+
+struct SimpleFlickrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
